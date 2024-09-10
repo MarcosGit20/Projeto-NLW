102fc917484b90ebd2f323c97470fc042df4f315
marcosgit20 <marcosaureliolinkpro@gmail.com>
2024-09-10 12:58:13 -0300

    fundamento 001

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+let meta = {
+    value: 'ler um livro por mes',    
+    checked: true,
+    
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[0].value)
