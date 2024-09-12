4e6335b77c2b033455972584e70974b436c11466
marcosgit20 <marcosaureliolinkpro@gmail.com>
2024-09-12 12:02:38 -0300

    deletar metas

--- index.js
+++ index.js
@@ -78,6 +78,29 @@ const metasAbertas = async ()=> {
     })
 }
 
+const deletarMetas = async () => {
+    const metasDesmarcadas = metas.map((meta) => {
+        return { value: meta.value, checked: false }
+    })
+    const itemsADeletar = await checkbox({
+        message: "Selecione item para deletar",
+        choices: [...metasDesmarcadas],
+        instructions: false,
+    })
+    if(itemsADeletar.length == 0) {
+        console.log("Nenhum item para deletar")
+        return
+    }
+
+    itemsADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+    })
+
+    console.log("Metas(s) deletada(a) com sucesso!")
+}
+
 const start = async () => {
 
     while (true) {
@@ -102,6 +125,10 @@ const start = async () => {
                     value: "abertas"
                 },
                 {
+                    name: "Deletar metas",
+                    value: "deletar"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -123,6 +150,9 @@ const start = async () => {
             case "abertas":
                 await metasAbertas()
                 break
+                case "deletar":
+                    await deletarMetas()
+                    break
             case "sair":
                 console.log("Até a próxima!")
                 return
